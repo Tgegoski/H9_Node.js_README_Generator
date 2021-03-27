eda504696299f15399c427b01af2650b8d18d9bb
Tammy Gegoski <tgegoski@gmail.com>
2021-03-26 20:21:56 -0400

    added more functions to index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('generate-readme');
-const utils = require('.utils');
-const generateMarkdown = generateMarkdown('./utils/generateMarkdown');
-const generateReadme = generateReadme('./package.json');
-
+const fs = require('inquirer');
+const utils = require('utils');
+const js_README_Generator = require('H9_Node.js_README_Generator/utils');
+const generateMarkdown = generateMarkdown('any');
 
 // TODO: Create an array of questions for user input
-const questions = [''];
+const promptUser = require('prompts');
+const inquirer = require('inquirer');
+const questions = [""]
+
+(async () => {
+    const response = await prompts(questions) 
+    }
+)
 
 inquirer
     .prompt([
@@ -47,23 +52,39 @@ inquirer
             message: 'What does the user need to know about contributing to the repo?',
             name: 'contribute',
         },
-    ]).then(answers => {
-
+    ])
+    .then(answers => {
+        Response.confirm === Response.answers
+            console.log('response');
     })
+    .catch(error => {
+        if(error.isError) {
+    }   else {
 
+    }
+    }),
 
 // TODO: Create a function to write README file
-// function writeToFile(README, data) {
-// }
-// ]).then((data) => {
-//     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-
-// fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-//       err ? console.log(err) : console.log('Success!')
-//  );
-// });
+    function importScripts() {
+    (inquirer, data) =>  {
+    fs.writeFile(nodeModule, generateMarkdown(data),
+        function (err) {
+            if (err) {
+                return console.log(err);
+        }
+})
+};
+    
 // TODO: Create a function to initialize app
-function init() { }
+        function init() {}
+            
+        function prompt(inquirer)
+            importScripts; ("inquirer"); {
+            console.log(JSON.stringify)(data, null," ");
+            data.getLiscense = getLiscense(data.liscense);
+            writeFile("./exampleREADME.md",data);
+    }
+}
 
 // Function call to initialize app
 init();
